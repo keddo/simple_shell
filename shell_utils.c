73097b218ebdb27d206852106884703cf5a418a4
keddo <adu.kedir.abdurahman@gmail.com>
2023-11-13 12:31:44 +0300

    checks if a command is found in the path

--- shell_utils.c
+++ shell_utils.c
@@ -87,3 +87,40 @@ void execute_command(char **tokenized_command, int command_type)
 		status = 127;
 	}
 }
+
+/**
+ * check_path - checks if a command is found in the PATH
+ * @command: command to be used
+ *
+ * Return: path where the command is found in, NULL if not found
+ */
+char *check_path(char *command)
+{
+	char **path_array = NULL;
+	char *temp, *temp2, *path_cpy;
+	char *path = _getenv("PATH");
+	int i;
+
+	if (path == NULL || _strlen(path) == 0)
+		return (NULL);
+	path_cpy = malloc(sizeof(*path_cpy) * (_strlen(path) + 1));
+	_strcpy(path, path_cpy);
+	path_array = tokenizer(path_cpy, ":");
+	for (i = 0; path_array[i] != NULL; i++)
+	{
+		temp2 = _strcat(path_array[i], "/");
+		temp = _strcat(temp2, command);
+		if (access(temp, F_OK) == 0)
+		{
+			free(temp2);
+			free(path_array);
+			free(path_cpy);
+			return (temp);
+		}
+		free(temp);
+		free(temp2);
+	}
+	free(path_cpy);
+	free(path_array);
+	return (NULL);
+}
