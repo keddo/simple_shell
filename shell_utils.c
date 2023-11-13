2bc1e00c151eb57b154061586e46c56377f0cdab
keddo <adu.kedir.abdurahman@gmail.com>
2023-11-13 12:34:07 +0300

    gets the value of an environment variable

--- shell_utils.c
+++ shell_utils.c
@@ -145,3 +145,29 @@ void (*get_func(char *command))(char **)
 	}
 	return (NULL);
 }
+
+/**
+ * _getenv - gets the value of an environment variable
+ * @name: name of the environment variable
+ *
+ * Return: the value of the variable as a string
+ */
+char *_getenv(char *name)
+{
+	char **my_environ;
+	char *pair_ptr;
+	char *name_cpy;
+
+	for (my_environ = environ; *my_environ != NULL; my_environ++)
+	{
+		for (pair_ptr = *my_environ, name_cpy = name;
+		     *pair_ptr == *name_cpy; pair_ptr++, name_cpy++)
+		{
+			if (*pair_ptr == '=')
+				break;
+		}
+		if ((*pair_ptr == '=') && (*name_cpy == '\0'))
+			return (pair_ptr + 1);
+	}
+	return (NULL);
+}
