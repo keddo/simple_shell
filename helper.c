d8136dd43956f84b16d4a51d0fffd91137573de1
keddo <adu.kedir.abdurahman@gmail.com>
2023-11-13 12:15:47 +0300

    counts string length

--- helper.c
+++ helper.c
@@ -83,3 +83,21 @@ void _strcpy(char *source, char *dest)
 		dest[i] = source[i];
 	dest[i] = '\0';
 }
+
+/**
+ *_strlen - counts string length
+ *@string: string to be counted
+ *
+ * Return: length of the string
+ */
+
+int _strlen(char *string)
+{
+	int len = 0;
+
+	if (string == NULL)
+		return (len);
+	for (; string[len] != '\0'; len++)
+		;
+	return (len);
+}
