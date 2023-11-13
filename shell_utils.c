770ade156e2d4c85a855a2936fdcc0a928591218
keddo <adu.kedir.abdurahman@gmail.com>
2023-11-13 12:33:06 +0300

    retrieves a function based on the command given and a mapping

--- shell_utils.c
+++ shell_utils.c
@@ -124,3 +124,24 @@ char *check_path(char *command)
 	free(path_array);
 	return (NULL);
 }
+
+/**
+ * get_func - retrieves a function based on the command given and a mapping
+ * @command: string to check against the mapping
+ *
+ * Return: pointer to the proper function, or null on fail
+ */
+void (*get_func(char *command))(char **)
+{
+	int i;
+	function_map mapping[] = {
+		{"env", env}, {"exit", quit}
+	};
+
+	for (i = 0; i < 2; i++)
+	{
+		if (_strcmp(command, mapping[i].command_name) == 0)
+			return (mapping[i].func);
+	}
+	return (NULL);
+}
