f8a89d8cba6b5cdc1c2cf2ec16840944062af369
keddo <adu.kedir.abdurahman@gmail.com>
2023-11-13 12:14:21 +0300

    removes new lines from a strings

--- helper.c
+++ helper.c
@@ -45,3 +45,24 @@ void print(char *string, int stream)
 	for (; string[i] != '\0'; i++)
 		write(stream, &string[i], 1);
 }
+
+/**
+ *remove_newline - removes new line from a string
+ *@str: string to be used
+ *
+ *
+ *Return: void
+ */
+
+void remove_newline(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] == '\n')
+			break;
+		i++;
+	}
+	str[i] = '\0';
+}
