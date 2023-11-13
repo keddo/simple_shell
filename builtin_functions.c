b423dc95136ddb384264afcf0d060449c8fae50d
keddo <adu.kedir.abdurahman@gmail.com>
2023-11-13 11:53:45 +0300

    prints the current environment

--- builtin_functions.c
+++ builtin_functions.c
@@ -1,1 +1,19 @@
 #include "shell.h"
+
+/**
+ *env - prints the current_environnement
+ *@tokenized_command: command entered
+ *
+ *Return: void
+ */
+
+void env(char **tokenized_command __attribute__((unused)))
+{
+	int i;
+
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		print(environ[i], STDOUT_FILENO);
+		print("\n", STDOUT_FILENO);
+	}
+}
