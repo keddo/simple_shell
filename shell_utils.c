5297bdb133d92d41c76d385797f2bc56200e50c9
keddo <adu.kedir.abdurahman@gmail.com>
2023-11-13 12:29:29 +0300

    determines the type of the command

--- shell_utils.c
+++ shell_utils.c
@@ -7,3 +7,41 @@
  * Description:
  * the extended functions for main.c
  */
+
+/** parse_command - determines the type of the command
+ * @command: command to be parsed
+ *
+ * Return: constant representing the type of the command
+ * Description -
+ * 		 EXTERNAL_COMMAND (1) represents commands like /bin/ls
+ *		 INTERNAL_COMMAND (2) represents commands like exit, env
+ *		 PATH_COMMAND (3) represents commands found in the PATH like ls
+ *		 INVALID_COMMAND (-1) represents invalid commands
+ */
+
+int parse_command(char *command)
+{
+	int i;
+	char *internal_command[] = {"env", "exit", NULL};
+	char *path = NULL;
+
+	for (i = 0; command[i] != '\0'; i++)
+	{
+		if (command[i] == '/')
+			return (EXTERNAL_COMMAND);
+	}
+	for (i = 0; internal_command[i] != NULL; i++)
+	{
+		if (_strcmp(command, internal_command[i]) == 0)
+			return (INTERNAL_COMMAND);
+	}
+	/* @check_path - checks if a command is found in the PATH */
+	path = check_path(command);
+	if (path != NULL)
+	{
+		free(path);
+		return (PATH_COMMAND);
+	}
+
+	return (INVALID_COMMAND);
+}
