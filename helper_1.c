a5953631323d9f889d5eb114ff7205e5111c4f97
keddo <adu.kedir.abdurahman@gmail.com>
2023-11-13 12:04:04 +0300

    computes segment of str1 which consists of charcaters not in str2

--- helper_1.c
+++ helper_1.c
@@ -41,3 +41,48 @@ char *_strcat(char *destination, char *source)
 	new_string[len_dest + len_source] = '\0';
 	return (new_string);
 }
+
+/**
+ *_strspn - gets the length of a prefix substring
+ *@str1: string to be searched
+ *@str2: string to be used
+ *
+ *Return: number of bytes in the initial segment of 5 which are part of accept
+ */
+
+int _strspn(char *str1, char *str2)
+{
+	int i = 0;
+	int match = 0;
+
+	while (str1[i] != '\0')
+	{
+		if (_strchr(str2, str1[i]) == NULL)
+			break;
+		match++;
+		i++;
+	}
+	return (match);
+}
+
+/**
+ *_strcspn - computes segment of str1 which consists of characters not in str2
+ *@str1: string to be searched
+ *@str2: string to be used
+ *
+ *Return: index at which a char in str1 exists in str2
+ */
+
+
+int _strcspn(char *str1, char *str2)
+{
+	int len = 0, i;
+
+	for (i = 0; str1[i] != '\0'; i++)
+	{
+		if (_strchr(str2, str1[i]) != NULL)
+			break;
+		len++;
+	}
+	return (len);
+}
