c1ba5406d2a6c9603ca5e90b682acafe131ff0e3
keddo <adu.kedir.abdurahman@gmail.com>
2023-11-13 11:45:37 +0300

    Add necessary builtin header files

--- shell.h
+++ shell.h
@@ -1,5 +1,15 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <errno.h>
+#include <dirent.h>
+#include <signal.h>
 
 #endif
