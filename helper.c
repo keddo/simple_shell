4c8557058544dc79456fa865afcab98b8fc437c1
keddo <adu.kedir.abdurahman@gmail.com>
2023-11-13 12:14:59 +0300

    copies a string to another buffer

--- helper.c
+++ helper.c
@@ -66,3 +66,20 @@ void remove_newline(char *str)
 	}
 	str[i] = '\0';
 }
+
+/**
+ *_strcpy - copies a string to another buffer
+ *@source: source to copy from
+ *@dest: destination to copy to
+ *
+ * Return: void
+ */
+
+void _strcpy(char *source, char *dest)
+{
+	int i = 0;
+
+	for (; source[i] != '\0'; i++)
+		dest[i] = source[i];
+	dest[i] = '\0';
+}
