92f745b2386d06c4750a17a70b89b010ff76eeb3
keddo <adu.kedir.abdurahman@gmail.com>
2023-11-13 12:01:03 +0300

    concatenates tow strings: returns address of hte new string

--- helper_1.c
+++ helper_1.c
@@ -20,3 +20,24 @@ int _strcmp(char *first, char *second)
 	}
 	return (first[i] - second[i]);
 }
+
+/**
+ *_strcat - concatenates two strings
+ *@destination: string to be concatenated to
+ *@source:  string to concatenate
+ *
+ * Return: address of the new string
+ */
+
+char *_strcat(char *destination, char *source)
+{
+	char *new_string =  NULL;
+	int len_dest = _strlen(destination);
+	int len_source = _strlen(source);
+
+	new_string = malloc(sizeof(*new_string) * (len_dest + len_source + 1));
+	_strcpy(destination, new_string);
+	_strcpy(source, new_string + len_dest);
+	new_string[len_dest + len_source] = '\0';
+	return (new_string);
+}
