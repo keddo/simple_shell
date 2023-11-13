b3de454dfc07e78f2f0876c97b8b55794df7086f
keddo <adu.kedir.abdurahman@gmail.com>
2023-11-13 12:06:07 +0300

    tokenizes a string

--- helper_2.c
+++ helper_2.c
@@ -1,1 +1,41 @@
 #include "shell.h"
+
+/**
+ *_strtok_r - tokenizes a string
+ *@string: string to be tokenized
+ *@delim: delimiter to be used to tokenize the string
+ *@save_ptr: pointer to be used to keep track of the next token
+ *
+ *Return: The next available token
+ */
+char *_strtok_r(char *string, char *delim, char **save_ptr)
+{
+	char *finish;
+
+	if (string == NULL)
+		string = *save_ptr;
+
+	if (*string == '\0')
+	{
+		*save_ptr = string;
+		return (NULL);
+	}
+
+	string += _strspn(string, delim);
+	if (*string == '\0')
+	{
+		*save_ptr = string;
+		return (NULL);
+	}
+
+	finish = string + _strcspn(string, delim);
+	if (*finish == '\0')
+	{
+		*save_ptr = finish;
+		return (string);
+	}
+
+	*finish = '\0';
+	*save_ptr = finish + 1;
+	return (string);
+}
