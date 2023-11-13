edaa88d71b910dd0e0eac1ac3337a4aac59a3d82
keddo <adu.kedir.abdurahman@gmail.com>
2023-11-13 12:30:53 +0300

    exectues a command based on it's type

--- shell_utils.c
+++ shell_utils.c
@@ -45,3 +45,45 @@ int parse_command(char *command)
 
 	return (INVALID_COMMAND);
 }
+
+/**
+ * execute_command - executes a command based on it's type
+ * @tokenized_command: tokenized form of the command (ls -l == {ls, -l, NULL})
+ * @command_type: type of the command
+ *
+ * Return: void
+ */
+void execute_command(char **tokenized_command, int command_type)
+{
+	void (*func)(char **command);
+
+	if (command_type == EXTERNAL_COMMAND)
+	{
+		if (execve(tokenized_command[0], tokenized_command, NULL) == -1)
+		{
+			perror(_getenv("PWD"));
+			exit(2);
+		}
+	}
+	if (command_type == PATH_COMMAND)
+	{
+		if (execve(check_path(tokenized_command[0]), tokenized_command, NULL) == -1)
+		{
+			perror(_getenv("PWD"));
+			exit(2);
+		}
+	}
+	if (command_type == INTERNAL_COMMAND)
+	{
+		func = get_func(tokenized_command[0]);
+		func(tokenized_command);
+	}
+	if (command_type == INVALID_COMMAND)
+	{
+		print(shell_name, STDERR_FILENO);
+		print(": 1: ", STDERR_FILENO);
+		print(tokenized_command[0], STDERR_FILENO);
+		print(": not found\n", STDERR_FILENO);
+		status = 127;
+	}
+}
